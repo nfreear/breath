614b4bf2941a00abd25273ae4de8244b7de38b3f
Nick Freear <nfreear@yahoo.co.uk>
2020-04-04 13:20:23 +0100

    Bug #3/#4, move CSS animation to separate file; tweak `app-about` CSS

--- docs/lib/service-worker.js
+++ docs/lib/service-worker.js
@@ -18,6 +18,7 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(
         [
+          `${PATH}/animate.css`,
           `${PATH}/app-about.html`,
           `${PATH}/app-about.js`,
           `${PATH}/icon.svg`,
