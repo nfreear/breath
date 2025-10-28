8dc1ee375e47695abe4f13a0f76333fe3e0e6fe6
Nick Freear <nfreear@yahoo.co.uk>
2025-10-28 15:37:18 +0000

    fix: Fix paths in `urlsToCache` JavaScript (#20)

--- docs/lib/urlsToCache.js
+++ docs/lib/urlsToCache.js
@@ -1,26 +1,27 @@
 const { location } = globalThis;
 
 export default function urlsToCache () {
-  const PATH = location.pathname.replace(/\/[^/]+$/, '') + '/lib'; // '/docs/service-worker.js'
+  const PATH = location.pathname.replace(/\/[^/]+$/, ''); // '/docs/service-worker.js'
 
   console.warn('sw.js ~ Location:', PATH, location, import.meta);
   return [
-    '/',
-    `${PATH}/../service-worker.js`,
-    `${PATH}/adjust-volume.js`,
-    `${PATH}/analytics.js`,
-    `${PATH}/animate.css`,
-    `${PATH}/app-about.html`,
-    `${PATH}/app-about.js`,
-    `${PATH}/icon.png`,
-    `${PATH}/icon.svg`,
-    `${PATH}/index.js`,
-    `${PATH}/sound.js`, // Was: 'audio.js'
-    `${PATH}/style.css`,
-    `${PATH}/urlsToCache.js`,
-    './sound/freesound-cactus2003-dark-103340-lq-clip.mp3', /// dur=44s;
-    './sound/freesound-pulswelle-baltic-339517-lq-clip.mp3', // dur=30s;
-    './sound/freesound-tec_studio-drone-353976-lq.mp3' // dur=34s; Was: `${PATH}/soundcloud-tec_studio-drone-353976-lq.mp3`,
+    `${PATH}/`,
+    `${PATH}/index.html`,
+    `${PATH}/service-worker.js`,
+    `${PATH}/lib/adjust-volume.js`,
+    `${PATH}/lib/analytics.js`,
+    `${PATH}/lib/animate.css`,
+    `${PATH}/lib/app-about.html`,
+    `${PATH}/lib/app-about.js`,
+    `${PATH}/lib/icon.png`,
+    `${PATH}/lib/icon.svg`,
+    `${PATH}/lib/index.js`,
+    `${PATH}/lib/sound.js`, // Was: 'audio.js'
+    `${PATH}/lib/style.css`,
+    `${PATH}/lib/urlsToCache.js`,
+    `${PATH}/sound/freesound-cactus2003-dark-103340-lq-clip.mp3`, /// dur=44s;
+    `${PATH}/sound/freesound-pulswelle-baltic-339517-lq-clip.mp3`, // dur=30s;
+    `${PATH}/sound/freesound-tec_studio-drone-353976-lq.mp3` // dur=34s; Was: `${PATH}/soundcloud-tec_studio-drone-353976-lq.mp3`,
     // '/offline.html'
   ];
 }
